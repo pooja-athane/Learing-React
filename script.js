e7cf55692871665f2dd540336e113b5fe6a048a2
pooja-athane <poojaathane2025@gmail.com>
2025-12-01 05:31:17 -0800

    adding the code of building UI wiothout React

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-const list = document.getElementById("list")
-const add = document.getElementById("add_button")
+// const list = document.getElementById("list")
+// const add = document.getElementById("add_button")
 
-let index = 1
+// let index = 1
 
-add.addEventListener("click", function () {
-    let element = document.createElement("li")
-    element.textContent = "Task #" + index++;
-    list.appendChild(element)
-})
+// add.addEventListener("click", function () {
+//     let element = document.createElement("li")
+//     element.textContent = "Task #" + index++;
+//     list.appendChild(element)
+// })
+
+
+// building the UI  without ReactJS
+
+function render(children) {
+    const app = document.getElementById("main")
+    app.appendChild(children)
+}
+
+function h1(children) {
+    const element = document.createElement("h1")
+    element.appendChild(children)
+    return element
+}
+function text(content) {
+    return document.createTextNode(content)
+}
+
+render(h1(text("UI witout React")))
